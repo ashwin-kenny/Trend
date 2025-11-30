b9761690c120e8da2905643de764ee2b931b64a6
ashwin-kenny <ashwinkennyak257@gmail.com>
2025-11-30 16:48:53 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -48,9 +48,9 @@ resource "aws_security_group" "ec2_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
