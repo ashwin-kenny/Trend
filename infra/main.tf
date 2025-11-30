3e80b86075db1b96dccc84a395763c4f0c434867
ashwin-kenny <ec2-user@ip-10-0-1-147.ec2.internal>
2025-11-30 11:50:52 +0000

    tf refresh

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "trendapp-tfstate"
+    key    = "tfstate/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
